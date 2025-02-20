bdccadeba993df06a86a5aae8f12a75080272591
kohallo <85572591+kohallo@users.noreply.github.com>
2025-02-20 17:16:29 +0000

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -142,7 +142,7 @@ def add_review(request):
             response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception as err:
-            print(f"Unexpected {err=}, {type(err)=}"):
+            print(f"Unexpected {err=}, {type(err)=}")
             return JsonResponse({
                 "status": 401, "message":
                 "Error posting the review"
