73a82380296388fea9b0a72f193e66bf18f228cd
kohallo <ohalloran.kieran@hotmail.com>
2025-02-26 11:39:56 -0500

    created car inventory

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,11 +31,15 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     'https://kieranohallo-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01\
+    .proxy.cognitiveclass.ai',
+    'https://kieranohallo-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01\
     .proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://kieranohallo-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01\
-    .proxy.cognitiveclass.ai'
+    .proxy.cognitiveclass.ai',
+    'https://kieranohallo-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01\
+    .proxy.cognitiveclass.ai'    
 ]
 
 REST_FRAMEWORK = {
